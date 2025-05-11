1d1d67504e4f2ba403d02ab39af2fb56e8b4d50a
GouriMangore1889 <mangoregouri@gmail.com>
2025-05-11 14:41:44 +0530

    Add files via upload

---
+++ isPowerofTwo.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<1)
+          return false;
+        else if(n==1)
+          return true;
+        else
+          {
+            while(n%2==0)
+              {
+                n=n/2;
+              }
+              if(n==1)
+                 return true;
+              else
+                 return false;
+          }
+    }
+};
+int main()
+  {
+  	int n;
+  	cout<<"enter number:";
+  	cin>>n;
+  	Solution S;
+  	bool result=S.isPowerOfTwo(n);
+	cout<<"result is "<<result;
+  }
